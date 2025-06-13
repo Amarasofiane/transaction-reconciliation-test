5914c2806ad7f49e5d168d50dcf6c2d4bf7fe367
AMARA sofiane <amarasofiane94@gmail.com>
2025-06-13 03:13:53 +0100

    init project and postgre

---
+++ src/main/java/com/aubay/transaction_reconciliation_test/TransactionReconciliationTestApplication.java
@@ -0,0 +1,13 @@
+package com.aubay.transaction_reconciliation_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionReconciliationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionReconciliationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aubay/transaction_reconciliation_test/TransactionReconciliationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aubay.transaction_reconciliation_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionReconciliationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
